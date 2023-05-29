ead7c4cff0ffd25081411716fc548ac3c7620f61
Subash-Jeevanantham <subashjeevanantham.alpharive@gmail.com>
2023-05-29 19:18:36 +0530

    update_29-05-2023_5

--- lib/screens/user/profile/profile.dart
+++ lib/screens/user/profile/profile.dart
@@ -1,5 +1,6 @@
 
 import 'package:coolwell_app/screens/user/basics/onboard/login.dart';
+import 'package:coolwell_app/screens/user/payment/payment_success.dart';
 import 'package:coolwell_app/screens/user/payment/payment_summary.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -294,9 +295,9 @@ class _Profile_ScreenState extends State<Profile_Screen> {
                       const SizedBox(height: 5.0,),
                       InkWell(
                         onTap: (){
-                          // Navigator.of(context).push(MaterialPageRoute(
-                          //     builder: (context) =>
-                          //         Payment_Summary_Screen()));
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) =>
+                                  PaymentSuccessScreen()));
                         },
                         child: Container(
                           padding: EdgeInsets.only(top: 10.0, bottom: 10.0, left: 20.0, right: 20.0),
